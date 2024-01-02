d8af0fffc7d1be9387583e7f0b73a19c3579b358
Taylor Nairn <taylornairn1@gmail.com>
2024-01-01 22:23:20 -0700

    Updated creep command and added functianallity for bot to read, send, and delete messages based on a helper function return value.

--- bot.js
+++ bot.js
@@ -5,7 +5,12 @@ import * as creep from "./commands/creep.js";
 config();
 
 const client = new Client({
-    intents: [GatewayIntentBits.Guilds],
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.MessageContent,
+    ],
 });
 
 function readyDiscord() {
@@ -16,10 +21,38 @@ client.once(Events.ClientReady, readyDiscord);
 client.login(process.env.TOKEN);
 
 async function handleInteraction(interaction) {
-    if (!interaction.isCommand()) return;
+    if (interaction.isCommand()) return;
     if (interaction.commandName === "creep") {
         await creep.execute(interaction);
     }
 }
 
 client.on(Events.InteractionCreate, handleInteraction);
+
+function findMessages(message) {
+    console.log("searching for a match: " + message);
+    return true;
+}
+
+client.on(Events.MessageCreate, async (message) => {
+    if (message.channel.name === "test") {
+        //eventually this channel should be splish-splash
+        if (message.author.bot) {
+            setTimeout(() => {
+                message.delete();
+            }, 10000);
+            return;
+        }
+        //call function that searches the rest of the channel for a matching message
+        const match = findMessages(message.content);
+        if (match) {
+            message.delete();
+            message.channel.send(
+                "Hello <@" +
+                    message.author +
+                    ">,\n" +
+                    "A matching message has been found, so yours has been deleted."
+            );
+        }
+    }
+});
